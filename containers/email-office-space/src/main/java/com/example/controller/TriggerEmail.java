9155e540834afc9107ab3db27740914d13b9f3ed
AnthonyAmanse <joe.amanse@obf.ateneo.edu>
2017-05-10 11:21:00 -0700

    Update email spring boot app

--- containers/email-office-space/src/main/java/com/example/controller/TriggerEmail.java
+++ containers/email-office-space/src/main/java/com/example/controller/TriggerEmail.java
@@ -1,13 +1,10 @@
 package com.example.controller;
 
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -24,17 +21,24 @@ public class TriggerEmail {
 
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Value("${spring.mail.username}")
+	private String sender;
+	
+	@Value("${trigger.mail.receiver}")
+	private String receiver;
+	
 
 	@RequestMapping(path = "/email", method = RequestMethod.POST)
 	private String send() {
 		MimeMessage mail = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(mail);
 		try {
-			helper.setTo("placeholder-gmail-receiver");
-			helper.setFrom("placeholder-gmail-sender");
-			helper.setReplyTo("placeholder-gmail-sender");
-			helper.setSubject("Office-Space");
-			helper.setText("Account Balance is now over $10,000");
+			helper.setTo(receiver);
+			helper.setFrom(sender);
+			helper.setReplyTo(sender);
+			helper.setSubject("Office-Space Notification");
+			helper.setText("Account Balance is now over $50,000");
 			mailSender.send(mail);
 			return "{\"message\": \"OK\"}";
 		} catch (MessagingException e) {
@@ -44,7 +48,7 @@ public class TriggerEmail {
 		}
 	}
 	
-	@RequestMapping(path = "/trigger", method = RequestMethod.POST)
+	@RequestMapping(path = "/triggertest", method = RequestMethod.POST)
 	private String trigger() {
 		RestTemplate rest = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
