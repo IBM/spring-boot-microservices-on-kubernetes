b108205b80f70177ad8a8312e9d019df718690f6
AnthonyAmanse <joe.amanse@obf.ateneo.edu>
2017-05-18 16:24:58 -0700

    Added email openwhisk action

--- containers/send-notification/src/main/java/com/example/controller/TriggerEmail.java
+++ containers/send-notification/src/main/java/com/example/controller/TriggerEmail.java
@@ -20,30 +20,51 @@ public class TriggerEmail {
 
 	@Autowired
 	private JavaMailSender mailSender;
-	
+
 	@Value("${spring.mail.username}")
 	private String sender;
-	
+
 	@Value("${trigger.mail.receiver}")
 	private String receiver;
-	
+
 	@Value("${trigger.slack.url}")
 	private String slack_url;
 
 	@Value("${trigger.slack.message}")
 	private String slack_message;
 
+	@Value("${trigger.email.url}")
+	private String email_url;
+	
+	@Value("${spring.mail.password}")
+	private String password;
+
 	@RequestMapping(path = "/email", method = RequestMethod.POST)
 	private String send() {
 		MimeMessage mail = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(mail);
+		
 		try {
-			helper.setTo(receiver);
-			helper.setFrom(sender);
-			helper.setReplyTo(sender);
-			helper.setSubject("Office-Space Notification");
-			helper.setText("Account Balance is now over $50,000");
-			mailSender.send(mail);
+			if (!email_url.isEmpty()) {
+				helper.setTo(receiver);
+				helper.setFrom(sender);
+				helper.setReplyTo(sender);
+				helper.setSubject("Office-Space Notification");
+				helper.setText("Account Balance is now over $50,000");
+				mailSender.send(mail);
+			}
+			else {
+				RestTemplate rest = new RestTemplate();
+				HttpHeaders headers = new HttpHeaders();
+				String server = email_url;
+				headers.add("Content-Type", "application/json");
+				headers.add("Accept", "*/*");
+				String json = "{\"text\": \"" + slack_message + "\",\"sender\": \"" + sender + "\",\"receiver\": \"" + receiver + "\",\"password\": \"" + password + "\",\"subject\": \"Office-Space Notification\"}";
+
+				HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
+				ResponseEntity<String> responseEntity = rest.exchange(server, HttpMethod.POST, requestEntity, String.class);
+			}
+
 
 			if (!slack_url.isEmpty()) {
 				RestTemplate rest = new RestTemplate();
@@ -56,7 +77,7 @@ public class TriggerEmail {
 				HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
 				ResponseEntity<String> responseEntity = rest.exchange(server, HttpMethod.POST, requestEntity, String.class);
 			}
-	        
+
 			return "{\"message\": \"OK\"}";
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -64,7 +85,7 @@ public class TriggerEmail {
 			return "{\"message\": \"Error\"}";
 		}
 	}
-	
+
 	@RequestMapping(path = "/triggertest", method = RequestMethod.POST)
 	private String trigger() {
 		RestTemplate rest = new RestTemplate();
@@ -73,7 +94,7 @@ public class TriggerEmail {
 		headers.add("Content-Type", "application/json");
 	    headers.add("Accept", "*/*");
 		String json = "{}";
-		
+
 		HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
 	    ResponseEntity<String> responseEntity = rest.exchange(server, HttpMethod.POST, requestEntity, String.class);
 	    return responseEntity.getBody();
